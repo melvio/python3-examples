00ba242466b0964649524920f7720f08e896b189
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-22 18:49:37 +0200

    -h option for --help

---
+++ snippets/popular_libraries/click-lib/click_cmd_with_help.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+import click
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help', "--huwh"]))
+def group_cmd():
+    pass
+
+
+@group_cmd.command(name="other_name",
+                   short_help="my short help")  # change the one line description
+def my_cmd():
+    click.echo("hi")
+
+
+print(my_cmd.short_help)  # my_short help
+print(my_cmd.name)  # other_name
+
+group_cmd()
+# ./click_cmd_with_help.py --huwh
+# Usage: click_cmd_with_help.py [OPTIONS] COMMAND [ARGS]...
+
+# Options:
+# -h, --help, --huwh  Show this message and exit.
+
+# Commands:
+#   other_name  my short help
