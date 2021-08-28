bc733d5f43197c5cab880a7899f04eda60ffe4e1
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-28 11:56:19 +0200

    click --editor

--- snippets/popular_libraries/click-lib/click_editor.py
+++ snippets/popular_libraries/click-lib/click_editor.py
@@ -4,15 +4,18 @@ import click
 
 
 @click.command()
-@click.option("--editor")
+@click.option("--editor", is_flag=True)
 def cli(editor):
-    comment = "# Every line below this comment represents an item on your checklist\n"
-    message = click.edit(text=comment)
-    if message is not None:
-        checklist_str = message.split(comment, maxsplit=1)[1]
-        checklist_list = checklist_str.split("\n")
+    if editor:
+        comment = "# Every line below this comment represents an item on your checklist\n"
+        message = click.edit(text=comment)
+        if message is not None:
+            checklist_str = message.split(comment, maxsplit=1)[1]
+            checklist_list = checklist_str.split("\n")
 
-        click.echo(checklist_list)
+            click.echo(checklist_list)
+
+    click.echo("no editor")
 
 
 if __name__ == "__main__":
