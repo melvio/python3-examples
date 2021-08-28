b3d99ca6cf66b39644be5fcf75fb4fcdf51d8759
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-28 06:54:03 +0200

    glom

---
+++ snippets/popular_libraries/glom-data/quickstart.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# <https://glom.readthedocs.io/en/latest/>
+import glom
+
+data = {"a": {"b": {"c": "d"}}}
+filter_result = glom.glom(data, spec="a.b.c")
+print(filter_result)  # d
+
+
+try:
+    glom.glom(data, spec='a.nope')
+except glom.core.PathAccessError as e:
+    print(e)
+# glom.core.PathAccessError: error raised while processing, details below.
+# Target-spec trace (most recent last):
+# - Target: {'a': {'b': {'c': 'd'}}}
+# - Spec: 'a.nope'
+# glom.core.PathAccessError: could not access 'nope', part 1 of Path('a', 'nope'), got error: KeyError('nope')
