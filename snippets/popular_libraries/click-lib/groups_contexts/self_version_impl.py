bc7becb1ff4023e8e8607a0b61c5175fd1454714
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-31 21:24:42 +0200

    exposing values

--- snippets/popular_libraries/click-lib/groups_contexts/self_version_impl.py
+++ snippets/popular_libraries/click-lib/groups_contexts/self_version_impl.py
@@ -23,5 +23,26 @@ def hello(value):
     click.echo("Hello, World!")
 
 
+def my_string(ctx: click.Context, param: click.Parameter, mystring_exposed_value: str):
+    click.echo(f"in my_string {mystring_exposed_value}")
+
+
+def my_int(ctx: click.Context, param: click.Parameter, myint_exposed_value: int):
+    click.echo(f"in my_int {myint_exposed_value}")
+
+
+@cli.command()
+@click.option("--string", "mystring", type=str, callback=my_string)
+@click.option("--int", "myint", type=int, callback=my_int)
+def cb(mystring, myint):
+    pass
+# $ ./self_version_impl.py cb --int 32 --string "hi there"
+# in my_int 32
+# in my_string hi there
+# $ ./self_version_impl.py cb --string "hi there" --int 32
+# in my_string hi there
+# in my_int 32
+
+
 if __name__ == '__main__':
     cli()
