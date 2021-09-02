d515159c33b740567d0f778290c561a51ff79b37
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-02 17:57:57 +0200

    click custom types

---
+++ snippets/popular_libraries/click-lib/click_types/custom_types.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import datetime
+
+import click
+
+import datetime
+from typing import Optional, Sequence, Any
+
+
+class EnhancedDate(click.DateTime):
+    name = "enhanceddate"
+
+    def __init__(self, formats: Optional[Sequence[str]] = None):
+        super().__init__(formats)
+
+    def convert(self, value: Any, param: Optional["Parameter"], ctx: Optional["Context"]) -> Any:
+        if value == "today":
+            return datetime.date.today()
+        if value == "tomorrow":
+            return datetime.date.today() + datetime.timedelta(days=1)
+
+        return super().convert(value, param, ctx)
+
+
+@click.command()
+@click.option("--param", type=EnhancedDate())
+def cli(param):
+    click.echo(param)
+    click.echo(type(param))
+
+cli()
