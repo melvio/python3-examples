9297fe230f6088376516273182509afa0338da35
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-12 16:10:51 +0200

    Add example: patching a mock with @mock.patch

---
+++ snippets/popular_libraries/pytestlib/code_to_test/patching_mock.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import requests
+from unittest import mock
+
+CALL_TIMES = 3
+
+URL = "https://api.github.com/zen"
+
+
+def get_github_data() -> set:
+    s = set()
+    for _ in range(CALL_TIMES):
+        text_response = requests.get(URL).text
+        s.add(text_response)
+
+    return s
+
+
+def mocked_requests_get(mock_url: str):
+    class MockResponse:
+        def __init__(self, text, status_code):
+            self.text = text
+            self.status_code = status_code
+
+    if mock_url == URL:
+        return MockResponse(text="test_text", status_code=200)
+
+
+@mock.patch("requests.get", side_effect=mocked_requests_get)
+def test_get_github_data(mock_get):
+    """patch mock with a function annotation"""
+    result = get_github_data()
+    assert result == {"test_text"}
+    assert len(mock_get.call_args_list) == CALL_TIMES
+
+
+def test_get_github_data2():
+    """Patch mock with a context manager"""
+    with mock.patch("requests.get", side_effect=mocked_requests_get) as mock_get:
+        result = get_github_data()
+        assert result == {"test_text"}
+        assert len(mock_get.call_args_list) == CALL_TIMES
