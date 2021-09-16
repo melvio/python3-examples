7276d2a2a93d080ea38dfd4809eedc846f36d701
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-16 17:43:03 +0200

    random.choices

--- snippets/standard_libraries/randomsecretslib/example_choices.py
+++ snippets/standard_libraries/randomsecretslib/example_choices.py
@@ -3,3 +3,6 @@ import random
 alist = [i for i in range(100)]
 # select 10 random items from a list
 print(random.sample(alist, k=10))
+
+short_list = ["A", "V", "P", "U"]
+print(random.choices(short_list, k=10))  # ['U', 'U', 'U', 'V', 'A', 'U', 'A', 'A', 'U', 'U']
