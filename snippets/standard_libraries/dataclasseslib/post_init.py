52b336800a287212af54319f5a9cebd6e4f546c0
Melvin <31448155+melvio@users.noreply.github.com>
2021-10-04 18:42:14 +0200

    __post_init__ example

---
+++ snippets/standard_libraries/dataclasseslib/post_init.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import dataclasses
+
+
+@dataclasses.dataclass()
+class LabMeasurement:
+    # https://docs.python.org/3/library/dataclasses.html#post-init-processing
+    name: str
+    value: float
+    unit: str = None
+
+    def __post_init__(self):
+        print(f"__post_init__ Received")
+        if self.name == "":
+            raise ValueError("name cannot be empty")
+
+
+urine_lab = LabMeasurement(name="albumine", value=110.1, unit="mg/24h")
